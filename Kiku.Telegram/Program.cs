674880f7623fe397743be28a99e31bbd2ceccb18
w1thluv <dimahudric@gmail.com>
2024-08-27 16:57:24 +0300

    с кайфрм жа

--- Kiku.Telegram/Program.cs
+++ Kiku.Telegram/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+string bebra = "";
+
+class Bebra {
+    public Bebra() {
+        System.Console.WriteLine("BEbra");
+    }
+}
